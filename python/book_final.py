4eee32ea9e81822bd4173ad4cddd20071d85f6be
Seneda <senedanassirali@gmail.com>
2017-08-12 16:41:55 +0100

    Commit

--- python/book_final.py
+++ python/book_final.py
@@ -43,7 +43,9 @@ class book(object):
         self.addLine("\\begin{document}")
 
     def addTableOfContents(self):
+        self.addLine("\\begin{KeepFromToc}")
         self.addLine("\\tableofcontents")
+        self.addLine("\\end{KeepFromToc}")
 
     def fix_string(self, string):
         string = string.replace("&", "\&")
@@ -62,6 +64,9 @@ class book(object):
         self.addLine("\\chapter*{%s}" % title)
         self.addLine("\\addcontentsline{toc}{chapter}{%s}" % title)
 
+    def addIntro(self, title, text):
+        self.addChapter(title)
+        self.addLine(text)
 
     def addColumn(self, date, title, text):
         self.addLine("\\Needspace{10\\baselineskip}")
@@ -87,20 +92,34 @@ import re
 
 
 def titleCase(string):
+    smallwords = ["a", "an", "the", "at", "by", "for", "in", "of", "on",
+                  "to", "up", "and", "as", "but", "or", "and", "nor", "for"]
+
     def repl_func(m):
         """process regular expression match groups for word upper-casing problem"""
         return m.group(1) + m.group(2).upper()
 
-    return re.sub("(^|\s)(\"\S|\S)", repl_func, string)
+    words = re.sub("(^|\s)(\"\S|\S)", repl_func, string)
+    words = words.split()
+    r = words[:1]
+    for word in words[1:]:
+        if word.lower() in smallwords:
+            r.append(word.lower())
+        else:
+            r.append(word)
+    return " ".join(r)
 
 
 def stripDateAtStart(string):
-    return string
-    string = string.strip('\n')
-
+    print(repr(string[:10]))
+    string = string.strip("\ufeff")
+    string = string.strip()
+    print(repr(string[:10]))
     end_of_first_bit = re.search("\.|\n", string).start()
     first_bit = string[:end_of_first_bit]
-    if len([a for a in first_bit if a.isdigit()]) >= 3:
+    print("Looking for date")
+    print(first_bit)
+    if (len([a for a in first_bit if a.isdigit()]) >= 3) or ("’0" in first_bit):
         # print(string[:60])
         # print([a for a in first_bit if a.isdigit()])
         print("Found date line: %d %s" % (end_of_first_bit, first_bit))
@@ -120,6 +139,9 @@ def main():
     b = book("book")
     b.addTableOfContents()
     text_files_dir = "/home/seneda/PycharmProjects/latexbook/doc_files/txts"
+
+    b.addIntro("Ian Pettyfer Brief Background",
+               open(path.join(text_files_dir, "Ian Pettyfer Brief Background.txt")).read())
 
     # For each file in the dir
 
